17c74966cc776944ea944953be6cafcbc0ce53f2
unknown <labeebmuath2003@gmail.com>
2023-08-09 13:13:49 +0300

    Random State added

--- salary.py
+++ salary.py
@@ -53,7 +53,7 @@ plt.show()
 # Splitting the dataset
 X = ds[['YearsExperience']]
 y = ds['Salary']
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 # Linear Regression
 model = LinearRegression()
